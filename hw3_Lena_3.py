d4723dd758923735f38da074ec1ebc07a35ba108
nimbus8888 <88199548+nimbus8888@users.noreply.github.com>
2021-08-24 14:16:45 -0600

    Update hw3_Lena_3.py

--- hw3_Lena_3.py
+++ hw3_Lena_3.py
@@ -4,14 +4,14 @@
 def my_func(arg1, arg2, arg3):
     if arg1 >= arg3 and arg2 >= arg3:
         return arg1 + arg2
-    elif arg1 >= arg2 and arg1 <= arg3:
+    elif arg1 >= arg2 and arg3 >= arg2:
         return arg1 + arg3
     else:
-        return (arg2 + arg3)
+        return arg2 + arg3
+
 
 # my_func(arg1 = int(input('Введите 1 - ')), arg2 = int(input('Введите 2 - ')), arg3 = int(input('Введите 3 - ')))
-result = my_func(arg1 = int(input('Введите 1 - ')), arg2 = int(input('Введите 2 - ')), arg3 = int(input('Введите 3 - ')))
-print(result)
+print(my_func(arg1 = int(input('Введите 1 - ')), arg2 = int(input('Введите 2 - ')), arg3 = int(input('Введите 3 - '))))
 
 
 
